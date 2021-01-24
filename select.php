4094febb6f37c47f0cd5828005d1d12b61229edc
nantachai <noom555-5@hotmail.com>
2021-01-24 19:49:55 +0700

    phpsql

---
+++ select.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "<table style='border: solid 1px black;'>";
+ echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+$servername = "localhost";
+$username = "webadmin";
+$password = "1234";
+$dbname = "myDB1";
+$conn = new mysqli ($servername, $username, $password, $dbname);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT id, firstname, lastname FROM MyGuests");
+    $stmt->execute();
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
+</body>
+</html>
